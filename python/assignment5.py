887706e392891869a1637e227a49bcce4957fc0c
Esther <estherqs516@gmail.com>
2024-07-07 16:46:22 +0100

    creating an encoded string or something like that...

---
+++ python/assignment5.py
@@ -0,0 +1,24 @@
+letters = {
+    "e": "3",
+    "A": "4",
+    "b": "6",
+    "B": "8",
+    "o": "0",
+    "I": "1",
+    "S": "5",
+    "f": "7",
+    "p": "9",
+    "Z": "2"
+}
+
+def replaceLetters(word, letters_dict):
+    replacedWord = ""
+    for char in word:
+        if char in letters_dict:
+            replacedWord += letters_dict[char]
+        else:
+            replacedWord += char
+    return replacedWord
+
+orgWord = "Hello, I an a Bot"
+print(replaceLetters(orgWord, letters))
--- python/example.py
+++ python/example.py
@@ -1,11 +1,42 @@
-def printStrContent(str):
-    [print(i) for i in str]
+def findNum(list, num):
+    for item in list:
+        if item == num:
+            return list.index(num)
+    return -1
 
-def revStr(str):
-    i = len(str) - 1
-    while i >= 0:
-        print(str[i])
-        i -= 1
+numbers = [1, 2, 3, 4, 5, 6]
+# print(findNum(numbers, 5))
 
+string = "Hello, world"
+def countLetter(str, letter):
+    count = 0
+    i = 0
+    while i < len(str):
+        if str[i] == letter:
+            count += 1
+        i += 1
+    return count
+    
+print(countLetter(string, "l"))
 
-revStr(input())
+def findLetterIndices(str, letter):
+    indices = []
+    i = 0
+    while i < len(str):
+        if str[i] == letter:
+            indices.append(i)
+        i += 1
+    return indices
+
+print(findLetterIndices(string, "l"))
+
+integers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def createNewList(list):
+    newList = []
+    i = 0
+    while i < len(list):
+        newList.append(list[i])
+        i += 1
+    return newList
+print(createNewList(integers)) 
